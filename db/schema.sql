dd2b690ab799f3dd6896b74498964efa9c6c67d2
roger-gsf <rogerferreira610@gmail.com>
2024-12-19 23:25:40 -0300

    fix: fixing db

--- db/schema.sql
+++ db/schema.sql
@@ -10,8 +10,7 @@ CREATE TABLE user (
     user_state VARCHAR(50) NOT NULL,
     user_city VARCHAR(100) NOT NULL,
     user_neighborhood VARCHAR(100) NOT NULL,
-    user_address_line VARCHAR(255) NOT NULL,
-    user_complement VARCHAR(255),
+    user_street_address VARCHAR(255) NOT NULL,
     user_avatar_path VARCHAR(500)
 );
 CREATE TABLE service_provider (
@@ -22,7 +21,7 @@ CREATE TABLE customer (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL REFERENCES user(id)
 );
-CREATE TABLE categories (
+CREATE TABLE category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     category_name ENUM(
         'Eletricista',
@@ -35,16 +34,14 @@ CREATE TABLE categories (
 );
 CREATE TABLE has_category (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    category_id INT NOT NULL,
-    service_provider_id INT NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES categories(id),
-    FOREIGN KEY (service_provider_id) REFERENCES service_provider(id)
+    service_provider_id INT NOT NULL REFERENCES service_provider(id),
+    category_id INT NOT NULL REFERENCES category(id)
 );
 CREATE TABLE scheduling (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT NOT NULL,
-    service_provider_id INT NOT NULL,
-    category_id INT NOT NULL,
+    service_provider_id INT NOT NULL REFERENCES service_provider(id),
+    customer_id INT NOT NULL REFERENCES customer(id),
+    category_id INT NOT NULL REFERENCES category(id),
     title VARCHAR(30) NOT NULL,
     service_description VARCHAR(255) NOT NULL,
     date_time DATETIME NOT NULL,
@@ -53,12 +50,8 @@ CREATE TABLE scheduling (
     day_shift ENUM('Manhã', 'Tarde', 'Noite') NOT NULL,
     status_ ENUM(
         'Em andamento',
+        'Aguardando validação',
         'Concluído',
-        'Cancelado',
-        'Aguardando validação'
-    ) NOT NULL,
-    FOREIGN KEY (service_provider_id) REFERENCES service_provider(id),
-    FOREIGN KEY (customer_id) REFERENCES customer(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id),
-    FOREIGN KEY (service_info_id) REFERENCES service_info(id)
+        'Cancelado'
+    ) NOT NULL
 );
