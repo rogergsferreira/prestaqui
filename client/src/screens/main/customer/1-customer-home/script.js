ab000cfaea1d4eb2fbe1cd76f17517ca4cd2517e
paulo-xavier <px568661@gmail.com>
2025-01-30 19:43:50 -0300

    :lipstick: Updating Main page Customer

--- client/src/screens/main/customer/1-customer-home/script.js
+++ client/src/screens/main/customer/1-customer-home/script.js
@@ -64,3 +64,39 @@ document.addEventListener('DOMContentLoaded', () => {
     loadUserInfo();
     document.querySelector('.new__scheduling').onclick = goToNewSchedulingPage;
 });
+
+
+// Javascript to handle the filter
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdown = document.querySelector(".dropdown");
+    const button = document.querySelector(".dropdown-btn");
+    const menu = document.querySelector(".dropdown-menu");
+    const radios = document.querySelectorAll(".dropdown-menu input[type='radio']");
+
+    // Open and close the menu when you select an option
+    button.addEventListener("click", function () {
+        let expanded = button.getAttribute("aria-expanded") === "true";
+        button.setAttribute("aria-expanded", !expanded);
+        dropdown.classList.toggle("open");
+    });
+
+    // Close the menu when you select an option
+    radios.forEach(radio => {
+        radio.addEventListener("change", function () {
+            setTimeout(() => {
+                button.textContent = document.querySelector(`label[for="${this.id}"]`).textContent;
+                dropdown.classList.remove("open");
+                button.setAttribute("aria-expanded", "false");
+            }, 150); 
+        });
+    });
+
+    // Close the dropdown when you click outside
+    document.addEventListener("click", function (event) {
+        if (!dropdown.contains(event.target)) {
+            dropdown.classList.remove("open");
+            button.setAttribute("aria-expanded", "false");
+        }
+    });
+});
